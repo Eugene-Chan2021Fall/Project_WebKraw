172c7b725d5027b589c3c5c0ab9afed2b2c0f883
Eugene-Chan2021Fall <eugene.chan@sjsu.edu>
2022-05-02 08:14:16 -0700

    git pull

--- website/view.py
+++ website/view.py
@@ -1,21 +1,24 @@
-from flask import Blueprint,render_template
+from flask import Flask, Blueprint,render_template
 
 views = Blueprint('views',__name__)
 
-headings = ("Product","Sites","Price tags")
+headings = ("Product", "Sites", "Price tags")
 data = (
-        ("Nike Air Frost One","Ebay","$150"),
-        ("Nike","Amazon","$100"),
-        ("Swoosh","Nike.com","$90"),
+        ("Nike Air Frost One", "Ebay", "$150"),
+        ("Nike", "Amazon", "$100"),
+        ("Swoosh", "Nike.com", "$90"),
 
 )
 
 
-@views.route('/')
+@views.route("/")
+
 def home():
     return render_template("home.html")
-    
 
-@views.route("/")
+   
+
+
+@views.route("/table")
 def table():
     return render_template("table.html",headings=headings,data=data)
